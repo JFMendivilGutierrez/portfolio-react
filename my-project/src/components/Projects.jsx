441eb3c73c674d0ada4c79b4542298865066316b
JFMendivilGutierrez <maingarenop@hotmail.com>
2023-07-23 16:52:46 -0700

    cambio a los projects

--- my-project/src/components/Projects.jsx
+++ my-project/src/components/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
         
         <div className="card ml-10 mr-10 my-10 mb-10 w-72 bg-slate-900 shadow-xl ">
     <figure className="px-2 pt-2">
-        <img src="../src/img/CHONITOS LOGO.jpg" alt="Shoes" className="rounded-xl" />
+        <img src="../src/img/CHONITOS LOGO.jpg" alt="" className="rounded-xl" />
     </figure>
     <div className="card-body items-center text-center text-slate-300">
         <h2 className="card-title">Chonitos BarberShop</h2>
@@ -20,39 +20,39 @@ function Projects() {
     
     <div className="card ml-10 mr-10 my-10 mb-10 w-72 bg-slate-900 shadow-xl ">
     <figure className="px-2 pt-2">
-        <img src="../src/img/CHONITOS LOGO.jpg" alt="Shoes" className=" text-slate-300 bg-slate-900 hover:bg-black active:bg-black" />
+        <img src="" alt="" className=" text-slate-300 bg-slate-900 hover:bg-black active:bg-black" />
     </figure>
     <div className="card-body items-center text-center text-slate-300">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title"></h2>
+        <p></p>
         <div className="card-actions">
-        <button className="btn text-slate-300 bg-slate-900 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-black active:bg-black">Buy Now</button>
+        <button className="btn text-slate-300 bg-slate-900 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-black active:bg-black">LINK</button>
         </div>
     </div>
     </div>
 
     <div className="card ml-10 mr-10 my-10 mb-10 w-72 bg-slate-900 shadow-xl ">
     <figure className="px-2 pt-2">
-        <img src="../src/img/CHONITOS LOGO.jpg" alt="Shoes" className="rounded-xl" />
+        <img src="" alt="" className="rounded-xl" />
     </figure>
     <div className="card-body items-center text-center text-slate-300">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title"></h2>
+        <p></p>
         <div className="card-actions">
-        <button className="btn text-slate-300 bg-slate-900 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-black active:bg-black">Buy Now</button>
+        <button className="btn text-slate-300 bg-slate-900 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-black active:bg-black">LINK</button>
         </div>
         </div>
         </div>
 
         <div className="card ml-10 mr-10 my-10 mb-10 w-72 bg-slate-900 shadow-xl ">
     <figure className="px-2 pt-2">
-        <img src="../src/img/CHONITOS LOGO.jpg" alt="Shoes" className="rounded-xl" />
+        <img src="" alt="" className="rounded-xl" />
     </figure>
     <div className="card-body items-center text-center text-slate-300">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title"></h2>
+        <p></p>
         <div className="card-actions">
-        <button className="btn text-slate-300 bg-slate-900 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-black active:bg-black">Buy Now</button>
+        <button className="btn text-slate-300 bg-slate-900 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 hover:bg-black active:bg-black">LINK</button>
         </div>
         </div>
         </div>
